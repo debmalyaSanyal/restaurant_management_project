731fa23e9f6bdd3d2b4a0d0582d8e193377e5150
debmalya sanyal <120792049+debmalyaSanyal@users.noreply.github.com>
2025-08-08 21:10:01 +0530

    25672

---
+++ home/__init__.py
@@ -0,0 +1,44 @@
+<!--HTML-->
+<div class = "Homepage">
+<h1>Welcome to the homepage</h1>
+<p>Making the web beaautigul with css</p>
+<button>Get Started</button>
+</div>
+
+<!--CSS-->
+<style>
+    body{
+        font-family : 'Segoe UI', sans-serif;
+        background-colour : #f8f9fa;
+        margin : 0;
+    }
+
+    .homepage{
+        text-align : centre;
+        padding : 100px 20px
+    }
+
+    .homepage h1{
+        font-size : 3rem;
+        colour :  #007bff ;
+    }
+
+    homepage p{
+        font-size : 1.2rem;
+        colour : #555;
+    }
+
+    .homepage button{
+        background : #007bff;
+        colour : white;
+        border : none;
+        padding : 12px 24px;
+        font-size : 1rem;
+        cursor : pointer;
+        border_radius : 8px;
+    }
+
+    .homepage button: hover{
+        background : #0056b3;
+    }
+</style>
